3cef9d3817a9e4c5bf9ec14dc8b17f8b70cb5b1b
shubham negi <negishubham93@gmail.com>
2022-09-12 21:46:24 +0530

    resolve some bugs

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   const port = process.env.PORT || 3001;
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
